34a078a72b71ace35d5a6cc1bc681b7318165058
AbrahamSanders <abraham.sanders@gmail.com>
2024-09-18 23:49:52 -0400

    Add `return_hanging_codes_chars` option to converter.chars_to_codes to support streaming use-cases

--- codec_bpe/converter.py
+++ codec_bpe/converter.py
@@ -1,7 +1,7 @@
 """
 Converter utility for converting discrete codec codes to and from unicode characters used for BPE tokenization.
 """
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Tuple
 import logging
 import numpy as np
 import torch
@@ -28,10 +28,7 @@ def codes_to_chars(
     for i in range(codes.shape[0]):
         codes[i] += unicode_offset + i*codebook_size
     codes = codes.T.reshape(-1)
-    if use_special_token_format:
-        chars = "".join([f"<{chr(c)}>" for c in codes])
-    else:
-        chars = "".join([chr(c) for c in codes])
+    chars = _flattened_shifted_codes_to_chars(codes, use_special_token_format)
     return chars
 
 def chars_to_codes(
@@ -40,16 +37,18 @@ def chars_to_codes(
     codebook_size: int,
     drop_inconsistent_codes: bool = True,
     drop_hanging_codes: bool = True,
+    return_hanging_codes_chars: bool = False,
     return_tensors: Optional[str] = None, 
     unicode_offset: int = UNICODE_OFFSET,
 ) -> Union[List[List[int]], np.ndarray, torch.Tensor]:
-    if chars[0] == "<":
+    use_special_token_format = chars[0] == "<"
+    if use_special_token_format:
         chars = chars.replace("<", "").replace(">", "")
     codes = np.array([ord(c) for c in chars])
     if drop_inconsistent_codes:
         codes = _drop_inconsistent_codes(codes, num_codebooks, codebook_size, unicode_offset)
     if drop_hanging_codes:
-        codes = _drop_hanging_codes(codes, num_codebooks, codebook_size, unicode_offset)
+        codes, begin_hanging, end_hanging = _drop_hanging_codes(codes, num_codebooks, codebook_size, unicode_offset)
     codes = codes.reshape(-1, num_codebooks).T
     for i in range(codes.shape[0]):
         codes[i] -= unicode_offset + i*codebook_size
@@ -57,7 +56,18 @@ def chars_to_codes(
         codes = codes.tolist()
     elif return_tensors == "pt":
         codes = torch.tensor(codes)
+    if return_hanging_codes_chars:
+        begin_hanging = _flattened_shifted_codes_to_chars(begin_hanging, use_special_token_format)
+        end_hanging = _flattened_shifted_codes_to_chars(end_hanging, use_special_token_format)
+        return codes, begin_hanging, end_hanging
     return codes
+
+def _flattened_shifted_codes_to_chars(codes: np.ndarray, use_special_token_format: bool) -> str:
+    if use_special_token_format:
+        chars = "".join([f"<{chr(c)}>" for c in codes])
+    else:
+        chars = "".join([chr(c) for c in codes])
+    return chars
 
 def _resolve_codebook(code: int, num_codebooks: int, codebook_size: int, unicode_offset: int) -> int:
     codebook = num_codebooks-1
@@ -95,22 +105,28 @@ def _drop_hanging_codes(
     num_codebooks: int,
     codebook_size: int,
     unicode_offset: int,
-) -> np.ndarray:
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     # first check for hanging codes at the beginning
+    begin_hanging = []
     while len(codes) > 0:
         actual_codebook = _resolve_codebook(codes[0], num_codebooks, codebook_size, unicode_offset)
         if actual_codebook == 0:
             break
+        begin_hanging.append(codes[0])
         codes = codes[1:]
         logger.info(f"Dropped hanging audio code (codebook {actual_codebook}) at beginning of sequence.")
     # then check for hanging codes at the end
+    end_hanging = []
     while len(codes) > 0:
         actual_codebook = _resolve_codebook(codes[-1], num_codebooks, codebook_size, unicode_offset)
         if actual_codebook == num_codebooks-1:
             break
+        end_hanging.append(codes[-1])
         codes = codes[:-1]
         logger.info(f"Dropped hanging audio code (codebook {actual_codebook}) at end of sequence.")
-    return codes
+    begin_hanging = np.array(begin_hanging)
+    end_hanging = np.array(end_hanging)[::-1]
+    return codes, begin_hanging, end_hanging
 
 
     
